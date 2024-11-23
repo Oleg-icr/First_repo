c899e8de20aa62786956125156cf119963d6aaff
Oleg-Icr <oleg.icr@gmail.com>
2024-11-23 18:17:17 +0100

    commin 2

--- main.py
+++ main.py
@@ -6,4 +6,26 @@ age = 42
 weight = 75.7
 is_student = False
 
-print(name, age, weight, is_student)
+print("Name:" , name, ", Age:" , age , ", Weight:", weight , ", Student:" , is_student)
+
+my_list = ["apple", "banana", "cherry"]
+print(my_list[1]) 
+my_list[1] = "blueberry"
+print(my_list)  
+my_list.append("orange")
+print(my_list)  
+my_list.insert(1, "banana")
+print(my_list) 
+my_list.remove("banana")
+print(my_list) 
+my_dict = {"name": "Олексій", "age": 30}
+print(my_dict["name"])
+print(my_dict["age"])
+print(my_dict)
+my_dict["age"] = 31
+print(my_dict)
+my_dict["email"] = "oleksiy@gmail.com"
+print(my_dict)
+del my_dict["email"]
+print(my_dict)
+    
