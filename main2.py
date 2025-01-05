97d373234256bb357e2f960e44595993a1679f9c
Oleg-Icr <oleg.icr@gmail.com>
2025-01-05 17:29:52 +0100

    modified:   main2.py

--- main2.py
+++ main2.py
@@ -1,11 +1,11 @@
 def factorial(n):
-    print("Виклик функції factorial з n = ", n)
+    print("Request function factorial with n = ", n)
     if n == 1:
-        print("Базовий випадок, n = 1, повернення 1")
+        print("Base case, n = 1, return 1")
         return 1
     else:
         result = n * factorial(n-1)
-        print("Повернення результату для n = ", n, ": ", result)
+        print("Return result for n = ", n, ": ", result)
         return result
 
 print(factorial(5))
