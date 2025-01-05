5a2af0bd7039118c6cfdd31d4eac0207a03d3b54
Oleg-Icr <oleg.icr@gmail.com>
2025-01-05 13:03:27 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -38,3 +38,15 @@ thistuple = ("apple", "banana", "cherry")
 print(thistuple)
 print("==================================================================")
 print("==================================================================")
+
+class Car:
+    vehicle_type = "Automobile"
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_info (self):
+        return f"{self.year} {self.make} {self.model}, Type: {Car.vehicle_type}"
+
